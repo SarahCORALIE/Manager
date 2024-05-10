8c75ce466056c0c0fc8332e2a64d6564ad810f99
Sarah CORALIE <sarah@findly.co>
2024-05-10 16:06:47 +0200

    Add : create fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Job;
+use App\Entity\Person;
+use DateTime;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +13,32 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        
+        // Create 20 Person
+        for ($i = 0; $i < 20; $i++) {
+            $person = new Person;
+            $person->setName('Prenom ' . $i);
+            $person->setFirstname('Nom ' . $i);
+            $person->setBirthdate(new DateTime('01/01/2000'));
+            $manager->persist($person);
+
+            $this->setReference("person".$i, $person);
+        }
+
+         // Create 20 Job
+         for ($i = 0; $i < 20; $i++) {
+            $job = new Job;
+            
+            $companyNames = array("companyName1",
+                "companyName2",
+                 "companyName3");
+            $job->setCompanyName($companyNames[rand(0,2)]);
+            $job->setPosition('position' . $i);
+            $job->setStartedAt(new DateTimeImmutable());
+            $job->setStopedAt(new DateTimeImmutable());
+            $job->setPerson($this->getReference('person'.rand(0,19)));
+            $manager->persist($job);
+        }
 
         $manager->flush();
     }
